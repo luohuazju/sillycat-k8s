14f686ce11b8036abaa4563c0d9e87352ec457fd
Hua Luo <hluo@lifesize.com>
2020-11-11 00:47:50 -0600

    add tester

---
+++ redis-sentinel/nodejs/tester-nodejs.js
@@ -0,0 +1,22 @@
+const Redis = require('ioredis');
+
+const REDIS_SENTINEL_SERVICE_HOST = 'redis.sillycat.net';
+const REDIS_SENTINEL_SERVICE_PORT = 26379;
+
+console.log('REDIS_SENTINEL_SERVICE_HOST:' + REDIS_SENTINEL_SERVICE_HOST);
+console.log('REDIS_SENTINEL_SERVICE_PORT:' + REDIS_SENTINEL_SERVICE_PORT);
+
+const redis = new Redis({
+    sentinels: [{ host: REDIS_SENTINEL_SERVICE_HOST, port: REDIS_SENTINEL_SERVICE_PORT }],
+    name: 'mymaster'
+});
+
+redis.set('foo', 'bar');
+
+redis.get('foo', function (err, res) {
+    if (!err) {
+        console.log(res);
+    } else {
+        console.log(err);
+    }
+});
