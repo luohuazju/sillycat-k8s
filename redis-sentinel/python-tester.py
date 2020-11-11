89594853e35083868c8ec47455c7d67cda55c57f
Hua Luo <hluo@lifesize.com>
2020-11-11 00:32:25 -0600

    python-test change

--- redis-sentinel/python-tester.py
+++ redis-sentinel/python-tester.py
@@ -9,11 +9,11 @@ redis_host = os.environ['REDIS_SENTINEL_SERVICE_HOST'] | 'redis.sillycat.net'
 redis_port = os.environ['REDIS_SENTINEL_SERVICE_PORT'] | 26379
 
 sentinel = Sentinel([(redis_host, int(redis_port))], socket_timeout=0.1)
-print sentinel.discover_master('mymaster')
-print sentinel.discover_slaves('mymaster')
+print(sentinel.discover_master('mymaster'))
+print(sentinel.discover_slaves('mymaster'))
 
 master = sentinel.master_for('mymaster', socket_timeout=0.1)
 master.set('foo', 'bar')
 
-print slave.get('foo')
-print master.get('carl')
+print(slave.get('foo'))
+print(master.get('carl'))
